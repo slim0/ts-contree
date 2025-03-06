673ef3d6d38ce82c9584b81c17e307a2241be37a
Slim <46485341+slim0@users.noreply.github.com>
2025-03-06 07:38:13 +0100

    feat: Prevent race condition when modifying waiting players application state (#1)

--- packages/backend/src/core/state.ts
+++ packages/backend/src/core/state.ts
@@ -1,8 +1,37 @@
+import { Mutex } from "async-mutex";
 import { Player } from "shared/src/types/players";
 
 type State = {
   waitingPlayers: Player[];
 };
+
 export const state: State = {
   waitingPlayers: [],
 };
+const waitingPlayersStateMutex = new Mutex();
+
+export async function retrieveWaitingPlayers(
+  count: number,
+): Promise<Player[] | undefined> {
+  const release = await waitingPlayersStateMutex.acquire();
+  try {
+    if (state.waitingPlayers.length >= count) {
+      return state.waitingPlayers.splice(0, count);
+    } else {
+      return undefined;
+    }
+  } finally {
+    release();
+  }
+}
+
+export async function pushNewWaitingPlayer(
+  newWaitingPlayer: Player,
+): Promise<void> {
+  const release = await waitingPlayersStateMutex.acquire();
+  try {
+    state.waitingPlayers.push(newWaitingPlayer);
+  } finally {
+    release();
+  }
+}
