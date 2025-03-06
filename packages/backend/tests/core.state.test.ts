c0b973be5a71a034e65498001766d601005479ca
Simon <simon.lyobard@gmail.com>
2025-03-06 09:24:50 +0100

    testcase: Add a missing testcase to test waiting players state modifications

---
+++ packages/backend/tests/core.state.test.ts
@@ -0,0 +1,29 @@
+import {
+  pushNewWaitingPlayer,
+  retrieveWaitingPlayers,
+  state,
+} from "backend/src/core/state";
+import { Player, PlayerUUID } from "shared/src/types/players";
+import { expect, test } from "vitest";
+
+test("update waiting player state", async () => {
+  const newPlayer1: Player = { uuid: "1" as PlayerUUID };
+  const newPlayer2: Player = { uuid: "2" as PlayerUUID };
+  const newPlayer3: Player = { uuid: "3" as PlayerUUID };
+
+  expect(await retrieveWaitingPlayers(3)).toBe(undefined);
+
+  await pushNewWaitingPlayer(newPlayer1);
+  await pushNewWaitingPlayer(newPlayer2);
+
+  expect(state.waitingPlayers.size).toBe(2);
+  expect(await retrieveWaitingPlayers(3)).toBe(undefined);
+
+  await pushNewWaitingPlayer(newPlayer3);
+
+  const players = await retrieveWaitingPlayers(3);
+
+  expect(players?.length).toBe(3);
+  expect(await retrieveWaitingPlayers(3)).toBe(undefined);
+  expect(state.waitingPlayers.size).toBe(0);
+});
