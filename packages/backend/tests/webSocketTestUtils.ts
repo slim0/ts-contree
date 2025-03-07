8977990601856552a76ef6d50b3d43392b1c8653
Simon <simon.lyobard@gmail.com>
2025-03-07 08:29:34 +0100

    testcase(backend): Add a testcase for end to end tests on the WebSocket server with a ping pong test

---
+++ packages/backend/tests/webSocketTestUtils.ts
@@ -0,0 +1,36 @@
+import http, { Server } from "node:http";
+import {createWebSocketServer} from "backend/src/server";
+
+export function startServer(port: number): Promise<Server> {
+  const server = http.createServer();
+  createWebSocketServer(server)
+
+    return new Promise<Server>((resolve) => {
+        server.listen(port, () => resolve(server));
+    });
+}
+
+export class TestWebSocket extends WebSocket {
+    waitUntil(state: "open" | "close", timeout: number = 1000): Promise<void> | void {
+      if (this.readyState === this.OPEN && state === "open") return;
+      if (this.readyState === this.CLOSED && state === "close") return;
+
+      return new Promise((resolve, reject) => {
+        let timerId: NodeJS.Timeout
+        const handleStateEvent = () => {
+          resolve();
+          clearTimeout(timerId);
+        };
+
+        this.addEventListener(state, handleStateEvent, { once: true });
+
+        timerId = setTimeout(() => {
+          this.removeEventListener(state, handleStateEvent);
+          if (this.readyState === this.OPEN && state === "open") return resolve();
+          if (this.readyState === this.CLOSED && state === "close") return resolve();
+
+          reject(new Error(`WebSocket did not ${state} in time.`));
+        }, timeout);
+      });
+    }
+  }
