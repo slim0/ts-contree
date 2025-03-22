9c458bc2efa7a0c4907b81bd26f5bc326e441ee0
Simon <simon.lyobard@gmail.com>
2025-03-22 12:31:30 +0100

    refactor(backend): Move code related to playGameMessage in the corresponding controllers file

--- packages/backend/src/controllers.ts/playGameMessage.ts
+++ packages/backend/src/controllers.ts/playGameMessage.ts
@@ -1,13 +1,133 @@
-import { Effect, Option, pipe } from 'effect'
+import { Effect, Match, Option, pipe } from 'effect'
+import { deckOf32Cards } from 'shared/src/messages/datas/cards'
 import {
   GameStartedMessage,
   PlayerStatusErrorMessage,
   WaitingForGameMessage,
 } from 'shared/src/messages/server/serverMessages'
-import { PlayerState } from '../core/database/players'
-import { gameStartedResponses, searchForNewGame } from '../core/game'
+import { distributeCards } from '../core/cards'
+import { GameState, createGame } from '../core/database/games'
+import { PartyState, createParty } from '../core/database/parties'
+import {
+  PlayerState,
+  retrieveWaitingPlayers,
+  setPlayerStatus,
+} from '../core/database/players'
+import { TeamState, createTeam } from '../core/database/teams'
 import { ServerResponse } from '../core/types'
 import { verifyPlayerStatus } from './common'
+
+type InitializedGameStates = {
+  game: GameState
+  teamA: TeamState
+  teamB: TeamState
+  party: PartyState
+  players: PlayerState[]
+}
+
+function gameStartedResponses(
+  initializedGameStates: InitializedGameStates,
+): Effect.Effect<Array<ServerResponse<GameStartedMessage>>> {
+  return pipe(
+    distributeCards(deckOf32Cards),
+    Effect.andThen((distributedCards) =>
+      Effect.forEach(initializedGameStates.players, (playerState, index) =>
+        Effect.succeed({
+          playerState,
+          data: {
+            _tag: 'GameStartedMessage' as const,
+            data: {
+              game: initializedGameStates.game.row,
+              teamA: initializedGameStates.teamA.row,
+              teamB: initializedGameStates.teamB.row,
+              party: initializedGameStates.party.row,
+              hand: distributedCards[index],
+            },
+          },
+        }),
+      ),
+    ),
+  )
+}
+
+function searchAvailablePlayers(
+  connectedPlayerState: PlayerState,
+): Effect.Effect<Option.Option<PlayerState[]>> {
+  return pipe(
+    Effect.promise(() => retrieveWaitingPlayers(3)),
+    Effect.andThen((maybePlayers) =>
+      Match.value(maybePlayers).pipe(
+        Match.when(undefined, () =>
+          pipe(
+            Effect.promise(() =>
+              setPlayerStatus(connectedPlayerState.uuid, 'waitingForGame'),
+            ),
+            Effect.andThen(() => Effect.succeed(Option.none())),
+          ),
+        ),
+        Match.orElse((players) =>
+          pipe(
+            Effect.promise(() =>
+              setPlayerStatus(connectedPlayerState.uuid, 'playing'),
+            ),
+            Effect.andThen(() =>
+              Effect.succeed(Option.some([...players, connectedPlayerState!])),
+            ),
+          ),
+        ),
+      ),
+    ),
+  )
+}
+
+function initializeGame(
+  players: PlayerState[],
+): Effect.Effect<InitializedGameStates> {
+  return pipe(
+    Effect.Do,
+    Effect.bind('teamA', () =>
+      Effect.promise(() =>
+        createTeam('Red Devil', players[0].uuid, players[2].uuid),
+      ),
+    ),
+    Effect.bind('teamB', () =>
+      Effect.promise(() =>
+        createTeam('Black Mamba', players[1].uuid, players[3].uuid),
+      ),
+    ),
+    Effect.bind('game', ({ teamA, teamB }) =>
+      Effect.promise(() => createGame(teamA.uuid, teamB.uuid)),
+    ),
+    Effect.bind('party', ({ game }) =>
+      Effect.promise(() => createParty(game.uuid)),
+    ),
+    Effect.andThen(({ game, teamA, teamB, party }) => ({
+      game,
+      teamA,
+      teamB,
+      party,
+      players,
+    })),
+  )
+}
+
+function searchForNewGame(
+  connectedPlayerState: PlayerState,
+): Effect.Effect<Option.Option<InitializedGameStates>> {
+  return pipe(
+    searchAvailablePlayers(connectedPlayerState),
+    Effect.andThen((maybePlayers) =>
+      Option.match(maybePlayers, {
+        onSome: (playersState) =>
+          pipe(
+            initializeGame(playersState),
+            Effect.andThen((states) => Effect.succeed(Option.some(states))),
+          ),
+        onNone: () => Effect.succeed(Option.none()),
+      }),
+    ),
+  )
+}
 
 export function onPlayGameMessage(
   connectedPlayerState: PlayerState,
